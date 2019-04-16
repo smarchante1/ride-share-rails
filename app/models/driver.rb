e7bb9c769c5d4917a1ac7ccabe18a38f11c44ce9
Margaret Finnan <msfinnan@gmail.com>
2019-04-16 15:41:50 -0700

    added total earnings method to driver.rb and displayed earnings on show

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,17 @@ class Driver < ApplicationRecord
 
   validates :vin, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def total_earnings
+    fee = 165
+    percentage = 0.80
+    total = (self.trips.sum { |trip| trip.cost - fee } * percentage) / 100
+    return "$#{total.round(2)}"
+  end
+
+  def first_published
+    books_with_year = self.books.where.not(publication_date: nil)
+    first_book = books_with_year.order(publication_date: :asc).first
+    return first_book.publication_date
+  end
 end

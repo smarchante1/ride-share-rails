63c6b8ab8f24d280429ab8b095c9ffaa6e050a3f
Stephanie Marchante <smarcha04@gmail.com>
2019-04-16 16:16:49 -0700

    added routes for passenger actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: "drivers#index" #REMEMBER TO CHANGE THIS TO HOMEPAGE
+  # root to: "drivers#index" #REMEMBER TO CHANGE THIS TO HOMEPAGE
 
+  # Routes for drivers
   get "/drivers", to: "drivers#index", as: "drivers" #index
 
   get "/drivers/new", to: "drivers#new", as: "new_driver"
@@ -12,6 +13,24 @@ Rails.application.routes.draw do
   patch "/drivers/:id", to: "drivers#update"
 
   delete "/drivers/:id", to: "drivers#destroy"
+
+  # Routes for passengers
+  root to: "passengers#index"
+
+  get "/passengers", to: "passengers#index", as: "passengers" #index
+
+  get "/passengers/new", to: "passengers#new", as: "new_passenger"
+  post "/passengers", to: "passengers#create"
+
+  get "/passengers/:id", to: "passengers#show", as: "passenger" #show
+
+  get "/passengers/:id/edit", to: "passengers#edit", as: "edit_passenger"
+  patch "/passengers/:id", to: "passengers#update"
+
+  delete "/passengers/:id", to: "passengers#destroy"
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

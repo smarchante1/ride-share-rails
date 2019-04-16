efc84a372dd980dce2a0f2775208d3b0d989bb75
Stephanie Marchante <smarcha04@gmail.com>
2019-04-16 16:40:31 -0700

    added form to create new passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -8,12 +8,29 @@ class PassengersController < ApplicationController
     @trips = @passenger.trips
   end
     
+  def new
+    @passenger = Passenger.new
+  end
       
+  def create
+    @passenger = Passenger.new(passenger_params)
+    is_successful = @passenger.save
 
+    if is_successful
+      redirect_to passengers_path
+    else
+      @passenger = passenger
+      render :new
+    end
+  end
 
+  
 
+  private
 
-
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_number)
+  end
 
 
 end

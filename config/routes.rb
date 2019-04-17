394dcd01e13513246c1a5235406cc92c75217f07
Margaret Finnan <msfinnan@gmail.com>
2019-04-17 11:24:14 -0700

    Added trips model, view, controller, made trip routes and nested trip routes in driver and passenger

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,19 @@
 Rails.application.routes.draw do
-  # root to: "drivers#index" #REMEMBER TO CHANGE THIS TO HOMEPAGE
+
+  # root to: "passengers#index" #REMEMBER TO CHANGE THIS TO HOMEPAGE
+
+  # Routes for trips # cut down some of these using only syntax
+  resources :trips
 
   # Routes for drivers
-  get "/drivers", to: "drivers#index", as: "drivers" #index
-
-  get "/drivers/new", to: "drivers#new", as: "new_driver"
-  post "/drivers", to: "drivers#create"
-
-  get "/drivers/:id", to: "drivers#show", as: "driver" #show
-
-  get "/drivers/:id/edit", to: "drivers#edit", as: "edit_driver"
-  patch "/drivers/:id", to: "drivers#update"
-
-  delete "/drivers/:id", to: "drivers#destroy"
+  resources :drivers do
+    resources :trips, only: [:index]
+  end
 
   # Routes for passengers
-  root to: "passengers#index"
-
-  get "/passengers", to: "passengers#index", as: "passengers" #index
-
-  get "/passengers/new", to: "passengers#new", as: "new_passenger"
-  post "/passengers", to: "passengers#create"
-
-  get "/passengers/:id", to: "passengers#show", as: "passenger" #show
-
-  get "/passengers/:id/edit", to: "passengers#edit", as: "edit_passenger"
-  patch "/passengers/:id", to: "passengers#update"
-
-  delete "/passengers/:id", to: "passengers#destroy"
-
-
-
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

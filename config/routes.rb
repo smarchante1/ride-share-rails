92eb3d42b49820a6a1b14ff4964d9328d9820c4e
Margaret Finnan <msfinnan@gmail.com>
2019-04-18 16:15:33 -0700

    can link to a trip and see details from drivers and trips shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-
-  # root to: "homepage#index"
+  root to: "homepage#index"
   resources :homepage
+
   resources :drivers
-  # Routes for trips # cut down some of these using only syntax
+
   resources :trips
 
-  # Routes for drivers
-
-  # Routes for passengers
   resources :passengers do
     resources :trips
   end
